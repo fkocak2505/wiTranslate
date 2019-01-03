9eca203f6b79c01b0827092296efcc5c6619890c
Fatih Koçak <fatih.kocak@cs.com.tr>
2019-01-03 10:57:37 +0300

    updated

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const rekognitionController = require("./controller/rekognitionController");
 
 const API_VERSION = "0.0.1";
 app.use(cors());
-app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.json({ limit: '250mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 
